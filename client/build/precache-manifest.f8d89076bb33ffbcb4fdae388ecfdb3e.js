5b8cec1e07c7abb612c56195b9f6a6c1af1d0a31
Navin Kumar <nk02031999@gmail.com>
2020-02-27 01:01:06 +0530

    added build folder

---
+++ client/build/precache-manifest.f8d89076bb33ffbcb4fdae388ecfdb3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e90c9b91eb1ed90ed6481b0017c61f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27487e520b3fb617451a",
+    "url": "/static/css/main.4e5a3a15.chunk.css"
+  },
+  {
+    "revision": "8e2b010667e1e4184db9",
+    "url": "/static/js/2.e5bb0218.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.e5bb0218.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27487e520b3fb617451a",
+    "url": "/static/js/main.96e40259.chunk.js"
+  },
+  {
+    "revision": "43db1d114dbf23c0568b",
+    "url": "/static/js/runtime-main.7744019e.js"
+  },
+  {
+    "revision": "afee2942e8f6f60f5a0b9200bcaf59b6",
+    "url": "/static/media/3rd.afee2942.jpg"
+  }
+]);
